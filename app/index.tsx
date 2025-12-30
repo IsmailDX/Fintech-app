cbd6381afccea7f6b34bae20360c6ad41c48ab6b
IsmailDX <ismaildxx@gmail.com>
2025-12-30 13:08:05 +0400

    setting up prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useAssets } from 'expo-asset'
+import { View, Text, StyleSheet } from 'react-native'
+import { Video } from 'expo-av'
 
 const index = () => {
-  return (
-    <View>
-      <Text>update</Text>
-    </View>
-  )
+    const [assets] = useAssets([require('@/assets/videos/intro.mp4')])
+    return (
+        <View style={styles.container}>
+            {assets && (
+                <Video
+                    source={{ uri: assets[0].uri }}
+                    style={{ width: 300, height: 300 }}
+                />
+            )}
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+})
 
 export default index
