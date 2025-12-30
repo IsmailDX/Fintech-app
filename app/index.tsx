f284a23988589665e62c1e36aa1dac41b5f9f631
IsmailDX <ismaildxx@gmail.com>
2025-12-30 17:46:21 +0400

    first page done

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { useEvent } from 'expo'
+import Colors from '@/constants/Colors'
+import { defaultStyles } from '@/constants/Styles'
+import { Link } from 'expo-router'
 import { useVideoPlayer, VideoView } from 'expo-video'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const Page = () => {
     const videoSource = require('../assets/videos/intro2.mp4')
@@ -18,35 +20,84 @@ const Page = () => {
                 style={styles.video}
                 contentFit="cover"
                 nativeControls={false}
-                allowsFullscreen={false}
+                fullscreenOptions={{
+                    enable: true,
+                }}
                 allowsPictureInPicture={false}
             />
-            <View style={{ marginTop: 80, padding: 20, zIndex: 1 }}>
+            <View style={{ marginTop: 80, padding: 20 }}>
                 <Text style={styles.header}>
                     Ready to change the way you money?
                 </Text>
             </View>
+
+            <View style={styles.buttons}>
+                <Link
+                    href={'/sign-in'}
+                    style={[
+                        defaultStyles.pillButton,
+                        { flex: 1, backgroundColor: Colors.dark },
+                    ]}
+                    asChild
+                >
+                    <TouchableOpacity>
+                        <Text
+                            style={{
+                                color: 'white',
+                                fontSize: 22,
+                                fontWeight: '500',
+                            }}
+                        >
+                            Sign in
+                        </Text>
+                    </TouchableOpacity>
+                </Link>
+
+                <Link
+                    href={'/sign-up'}
+                    style={[
+                        defaultStyles.pillButton,
+                        { flex: 1, backgroundColor: '#fff' },
+                    ]}
+                    asChild
+                >
+                    <TouchableOpacity>
+                        <Text
+                            style={{
+                                fontSize: 22,
+                                fontWeight: '500',
+                            }}
+                        >
+                            Sign up
+                        </Text>
+                    </TouchableOpacity>
+                </Link>
+            </View>
         </View>
     )
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'space-between',
     },
     video: {
+        width: '100%',
+        height: '100%',
         position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
     },
     header: {
         fontSize: 36,
         fontWeight: '900',
         textTransform: 'uppercase',
         color: 'white',
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 20,
+        marginBottom: 60,
+        paddingHorizontal: 20,
     },
 })
 
